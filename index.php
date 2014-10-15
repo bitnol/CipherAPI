802c63c48e92594808cbddbc8aa3f261a5dca568
Akhilesh Chandra Gupta <akhilesh@bitnol.com>
2014-10-15 21:31:56 +0530

    Setting Geographical language setting

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	$info_url = "http://www.youtube.com/get_video_info?el=detailpage&asv=3&video_id=".$video_id;
 	
 	// youtube webpage url formation
-	$yt_url = 'http://www.youtube.com/watch?v='.$video_id;
+	$yt_url = 'http://www.youtube.com/watch?v='.$video_id.'&persist_gl=1&gl=US';
 
 	// get the contents from the url
 	$raw_data = file_get_contents($info_url);
